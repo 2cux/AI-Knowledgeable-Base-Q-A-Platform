7dc9095994a803ad6e8c8c2ffbc3a62f02622b0a
Lenovo <cb1690015395@qq.com>
2026-04-15 08:55:50 +0800

    修复了一些已知的错误

--- src/main/java/com/example/aikb/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/aikb/exception/GlobalExceptionHandler.java
@@ -13,11 +13,6 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler(BusinessException.class)
-    public Result<Void> handleBusinessException(BusinessException ex) {
-        return Result.fail(ex.getCode(), ex.getMessage());
-    }
-
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public Result<Void> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
         String message = ex.getBindingResult()
@@ -47,5 +42,11 @@ public class GlobalExceptionHandler {
     public Result<Void> handleBusinessException(BusinessException ex) {
         log.warn("Business exception, code={}, message={}", ex.getCode(), ex.getMessage());
         return Result.fail(ex.getCode(), ex.getMessage());
+    }
+
+    @ExceptionHandler(Exception.class)
+    public Result<Void> handleException(Exception ex) {
+        log.error("Unhandled exception", ex);
+        return Result.fail(50000, "Internal server error");
     }
 }
