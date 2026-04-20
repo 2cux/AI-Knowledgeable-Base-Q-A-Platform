a63abc0eb2f9eab348f65e96d3850e09e7a5f625
2cux <15972809010@163.com>
2026-04-20 14:02:27 +0800

    chore: 忽略构建产物

--- target/classes/db/migration/V2__add_email_to_user.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE `user`
-    ADD COLUMN `email` VARCHAR(128) NOT NULL DEFAULT '' COMMENT 'User email' AFTER `password_hash`;
--- target/classes/db/migration/V5__add_document_chunk_indexes.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE `document_chunk`
-    ADD INDEX `idx_chunk_document_index` (`document_id`, `chunk_index`);
